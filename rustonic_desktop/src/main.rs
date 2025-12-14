bb10da0d72e456cd9b3bf2f9bc2b8e5b6a9dbcc3
Jonathan Croteau-Dicaire <jonathan.croteau.dicaire@gmail.com>
2025-12-13 23:49:11 -0500

    template with slint desktop and in webapp with a shared lib

--- rustonic_desktop/src/main.rs
+++ rustonic_desktop/src/main.rs
@@ -1,8 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rustonic_shared::*;
 use std::error::Error;
-
-slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
