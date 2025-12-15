11e24263740bf65ad7b7df5cf8ea6ac05f030d32
Jonathan Croteau-Dicaire <jonathan.croteau.dicaire@gmail.com>
2025-12-14 22:36:19 -0500

    add basic api test for subsonic

--- rustonic_desktop/src/main.rs
+++ rustonic_desktop/src/main.rs
@@ -1,18 +1,46 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-slint::include_modules!();
+use rustonic_shared::SubsonicApi;
+use std::{error::Error, sync::Arc};
+use tokio::runtime::Builder;
 
-use std::error::Error;
+slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
+    let tokio_runtime = Arc::new(
+        Builder::new_multi_thread()
+            .worker_threads(4)
+            .enable_all()
+            .build()
+            .expect("Failed to create Tokio runtime"),
+    );
+
+    let api = SubsonicApi::new(
+        "".to_owned(),
+        "".to_owned(),
+        "".to_owned(),
+        "Rustonic Desktop".to_owned(),
+    )?;
+
+    ui.on_request_song_list(move || {
+        let api_clone = api.clone();
+        let runtime_clone = tokio_runtime.clone();
+
+        slint::spawn_local(async move {
+            let handle = runtime_clone.spawn(async move {
+                println!("Starting async ping...");
+                api_clone.ping().await
+            });
+
+            match handle.await {
+                Ok(Ok(())) => println!("Ping successful!"),
+                Ok(Err(e)) => eprintln!("Ping failed: {}", e),
+                Err(e) => eprintln!("Tokio task panicked: {}", e),
+            }
+        })
+        .unwrap();
     });
 
     ui.run()?;
--- rustonic_shared/src/lib.rs
+++ rustonic_shared/src/lib.rs
@@ -1,1 +1,61 @@
+use std::error::Error;
 
+use reqwest::{Client, Url};
+
+const VERSION: &str = "1.16.1";
+const BASE_PATH: &str = "rest";
+
+#[derive(Debug, Clone)]
+pub struct SubsonicApi {
+    username: String,
+    password: String,
+    url: Url,
+    client: Client,
+    app_name: String,
+}
+
+impl SubsonicApi {
+    pub fn new(
+        username: String,
+        password: String,
+        url: String,
+        app_name: String,
+    ) -> Result<SubsonicApi, Box<dyn Error>> {
+        Ok(SubsonicApi {
+            username,
+            password,
+            url: Url::parse(url.as_str())?,
+            client: Client::new(),
+            app_name,
+        })
+    }
+
+    pub async fn ping(&self) -> Result<(), Box<dyn Error + Send + Sync>> {
+        let mut ping_url = self.url.clone();
+
+        let salt = "abcdefghij";
+        let compute_string = &mut self.password.clone();
+        compute_string.push_str(salt);
+        let token = md5::compute(compute_string);
+
+        ping_url
+            .path_segments_mut()
+            .map(|mut segs| {
+                segs.pop_if_empty().push(BASE_PATH).push("ping");
+            })
+            .map_err(|_| "failed to set path segments")?;
+
+        ping_url
+            .query_pairs_mut()
+            .append_pair("u", &self.username)
+            .append_pair("t", format!("{:x}", token).as_str())
+            .append_pair("s", salt)
+            .append_pair("v", VERSION)
+            .append_pair("c", &self.app_name);
+
+        let response = self.client.get(ping_url).send().await?;
+        println!("{} {:?}", response.status(), response.bytes().await?);
+
+        Ok(())
+    }
+}
